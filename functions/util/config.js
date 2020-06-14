82ecec7717e2afee3b6a109bfd96939d1ba31a23
Kamrul Hossain <kh2857@columbia.edu>
2020-06-14 17:57:06 -0400

    Split the code into multiple files and separated concerns for better readability and maintainability.

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBATJhsS3dp_CYr9XlOv_ut8K7R2szKB7k",
+    authDomain: "socialapp-64e95.firebaseapp.com",
+    databaseURL: "https://socialapp-64e95.firebaseio.com",
+    projectId: "socialapp-64e95",
+    storageBucket: "socialapp-64e95.appspot.com",
+    messagingSenderId: "907634317016",
+    appId: "1:907634317016:web:c2ed0904d70d71e5a229ad",
+    measurementId: "G-1Q37L9FRB4"
+};
