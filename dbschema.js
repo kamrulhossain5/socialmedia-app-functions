dbcfa3fa2ca9b8ae5baa0e6e880f0d6f243bfc42
Kamrul Hossain <kh2857@columbia.edu>
2020-06-16 17:28:20 -0400

    Created a route for adding user profile details.

--- dbschema.js
+++ dbschema.js
@@ -1,11 +1,46 @@
 let db = {
-    posts: [
-        {
-            userHandle: 'user',
-            body: 'this is the post body',
-            createdAt: '2020-06-11T17:05:25.553Z',
-            likeCount: 5,
-            commentCount: 2
-        }
-    ]
-}
+  users: [
+    {
+      userId: "dh23ggj5h32g543j5gf43",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2019-03-15T10:59:52.798Z",
+      imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+      bio: "Hello, my name is user, nice to meet you",
+      website: "https://user.com",
+      location: "New York, NY"
+    }
+  ],
+  posts: [
+    {
+      userHandle: "user",
+      body: "this is the post body",
+      createdAt: "2020-06-11T17:05:25.553Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: "N43KJ5H43KJHREW4J5H3JWMERHB",
+    email: "user@email.com",
+    handle: "user",
+    createdAt: "2019-03-15T10:59:52.798Z",
+    imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+    bio: "Hello, my name is user, nice to meet you",
+    website: "https://user.com",
+    location: "London, UK"
+  },
+  likes: [
+    {
+      userHandle: "user",
+      postId: "hh7O5oWfWucVzGbHH2pa"
+    },
+    {
+      userHandle: "user",
+      postId: "3IOnFoQexRcofs5OhBXO"
+    }
+  ]
+};
--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,27 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-const app = require('express')();
+const app = require("express")();
 
-const FBAuth = require('./util/fbAuth')
+const FBAuth = require("./util/fbAuth");
 
-const { getAllPosts, postOnePost } = require('./handlers/posts')
-const { signup, login, uploadImage } = require('./handlers/users')
+const { getAllPosts, postOnePost } = require("./handlers/posts");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require("./handlers/users");
 
 // Post routes
-app.get('/posts', getAllPosts);
-app.post('/post', FBAuth, postOnePost)
+app.get("/posts", getAllPosts);
+app.post("/post", FBAuth, postOnePost);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 // Users routes
-app.post('/signup', signup);
-app.post('/login', login)
-app.post('/user/image', FBAuth, uploadImage)
+app.post("/signup", signup);
+app.post("/login", login);
 
 exports.api = functions.https.onRequest(app);
